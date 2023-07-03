f4ecd29af360b97f1cb5e903841695ca82e9326b
aftermellon <mykyta.ponomarenko@gmail.com>
2023-07-03 14:16:17 +0300

    completed Numbers and Strings

--- Chapter4/strings_numbers.py
+++ Chapter4/strings_numbers.py
@@ -33,6 +33,14 @@ print(spam[:])
 
 print("\n")
 
+#String methods
+print(spam)
+print(spam.find('pa'))
+print(spam.find('is_not_there'))
+print(spam.replace('pa', 'xyz'))
+
+print("\n")
+
 #Numbers (immutable), math and random
 import math
 print(math.pi)
@@ -42,4 +50,26 @@ import random
 print(random.random)
 print(random.choice([1,2,3,4]))
 
+print("\n")
 
+# More String Methods
+line = 'aaa, bbb, cc, d'
+test = line.split(',') # split with delimiter into list of substrings
+print(test[0])
+print(spam.upper())
+print(spam.isalpha()) # Content test - characters in a string are alphabetic (letters) or not
+print(spam.isdigit())
+
+line = 'aaa, bbb, cc, d\n'
+print(line)
+line = line.rstrip() # Remove whitespace chars on the rigth
+print(line)
+
+print("\n")
+# String formatting
+option_one = '%s, eggs and %s' % ('text1', 'text2')
+print(option_one)
+option_two = '{0}, eggs and {1}'.format('text3', 'text4')
+print(option_two)
+print('%s, %s' % (spam, line))
+print('{0}, {1}'.format(spam, line))
